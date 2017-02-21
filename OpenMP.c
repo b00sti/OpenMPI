4d078c8e1c81b8ad6b087af7e3964d2f10eeb134
Dominik Pawlik <pawlik.dominik@gmail.com>
2017-02-21 13:24:54 +0100

    Update OpenMP.c

--- OpenMP.c
+++ OpenMP.c
@@ -13,7 +13,7 @@ main(int argc, char** argv) {
 
   omp_set_num_threads(argv[1]);
 
-  printf("Przygotowywanie tablicy a... \n");
+  printf("Preparing tab a... \n");
   start_time();
   //data preparing (tab a and b with random numbers)
   for( i=0; i<NBR_OF_ELEMENTS; i++){
@@ -21,27 +21,27 @@ main(int argc, char** argv) {
   }
   stop_time();
   //print elapsed time
-  print_time("Uplynelo:");
+  print_time("Elapsed:");
 
   start_time();
-  printf("Przygotowywanie tablicy b... \n");
+  printf("Preparing tab b... \n");
   for( i=0; i<NBR_OF_ELEMENTS; i++){
     b[i]=1.0/rand();
   }
   stop_time();
   //print elapsed time
-  print_time("Uplynelo:");
+  print_time("Elapsed:");
 
-  printf("Rozpoczeto dodawanie... \n");
-  //dodawanie
+  printf("\nAdding started... \n");
+  //adding
   start_time();
     #pragma omp parallel for
   for( i=0; i<NBR_OF_ELEMENTS; i++) {
     c[i] = a[i] + b[i];
   }
-  printf("Zakonczono dodawanie... \n");
+  printf("Adding finished... \n");
 
   stop_time();
   //print elapsed time
-  print_time("Czas dodawania:");
+  print_time("Adding time:");
 }
